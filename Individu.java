d53eb7a1a91db26f646f673a4f1eb5e26c6fff21
vaddrep <77686027+vaddrep@users.noreply.github.com>
2021-03-09 15:33:11 +0100

    Update Individu.java

--- Individu.java
+++ Individu.java
@@ -1,8 +1,9 @@
 public class Individu {
+	public static tailleEquipement = 2;
 	//caractéristiques de l'individu
 	public String nom;
 	public int pv, mana, armurePhysique = 0, armureMagique = 0, agilite, force, constitution, sagesse, perception, charisme, chance;
-	//public /*classe équipement */ 
+	private Equipement[tailleEquipement] equipement, int nbMainL = 2; 	//désigne l'équipement et le nombre de main libres restantes
 	
 	public Individu(String n, int ag, int f, int co, int s, int p, int ca, int ch) {
 		nom = n;
@@ -25,7 +26,35 @@ public class Individu {
 		return sagesse*2;
 	}
 	
-	/*public int attaqueNormaleForce() {
-		
-	}*/
+	public void equiper(Equipement e) {
+		if(nbMainL <= e.nbMain) {
+			nbMainL = nbMainL - e.nbMain;
+		}
+		for(int i = 0; i < tailleEquipement; i++) {
+			if(equipement[i] == null) {
+				equipement[i] = e;
+				return
+			}
+		}
+		System.out.println("erreur: impossible d'équiper l'équipement");
+		System.exit(-1);
+	}
+	
+	public void desequiper(Equipement e) {
+		for(int i = 0; i < tailleEquipement; i++) {
+			if(equipement[i].nom == e.nom) {
+				equipement[i] = null;
+				nbMainL = nbMainL + e.nbMain;
+				return
+			}
+		}
+		System.out.println("erreur: impossible de déséquiper l'équipement");
+		System.exit(-1);
+	}
+	
+	public void afficherEquipement() {
+		for(int i = 0; i < tailleEquipement; i++) {
+			System.out.println("Arme " + i + " : " + i.nom);
+		}
+	}
 }
