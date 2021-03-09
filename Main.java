60fa95e21de1c9fd25b0efb998a95ceedf27a000
SirShok <47355191+SirShok@users.noreply.github.com>
2021-03-09 13:37:51 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,7 +2,23 @@
 public class Main {
 
 	public static void main(String[] args) {
-
+		Path chemin = Paths.get("DossierConfig\\Config.txt");
+		FileReader fichier = new FileReader("DossierConfig\\Config.txt");
+		BufferedReader lecteurFichier = new BufferedReader(fichier);
+		String ligne;
+		String[] mot;
+		ArrayList<Monstre> bestiaire = new ArrayList<Monstre>();
+		while(((ligne = lecteurFichier.readLine()) != null)) {
+			System.out.println(ligne);
+			mot=ligne.split(" ");
+			Monstre m = new Monstre(mot[0],Integer.parseInt(mot[1]),Integer.parseInt(mot[1]));
+			bestiaire.add(m);
+		}
+		for (Monstre E : bestiaire) {
+			E.PrintMonstre(E);
+		}
+		lecteurFichier.close();
+		}
 	}
 
 }
