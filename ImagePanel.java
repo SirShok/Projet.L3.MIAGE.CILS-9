3a663f35a9c392c54fe5cc71dba734ed589dbd58
CrepeAuChocolat <77682600+CrepeAuChocolat@users.noreply.github.com>
2021-03-14 14:46:58 +0100

    Update ImagePanel.java

--- ImagePanel.java
+++ ImagePanel.java
@@ -3,23 +3,23 @@ import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 import javax.swing.JPanel;
 
+//Classe permettant de creer des JPanel avec une image de fond
 public class ImagePanel extends JPanel {
 	
 	private BufferedImage img;
 	
-	public ImagePanel(){
-		
-	}
-	
+	//Constructeur
 	public ImagePanel(BufferedImage i){
 		img=i;
 	}
 	
+	//Renvoie la taille du panel
 	@Override
 	public Dimension getPreferredSize(){
 		return new Dimension(img.getWidth(null), img.getHeight(null));
 	}
 	
+	//Dessine l'image de fond du panel
 	@Override
 	public void paintComponent(Graphics g){
 		super.paintComponent(g);
