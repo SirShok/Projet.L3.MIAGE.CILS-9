65092f82a8881dd08d8daf595ea6a6dc724f71a2
SirShok <47355191+SirShok@users.noreply.github.com>
2021-03-14 15:09:33 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,5 +6,10 @@ public class Main {
 		new Equipement("dague en fer", 6, 0, 0, 4, 1, 2), new Equipement("arc en érable avec flèches en fer", 6, 0, 0, 2, 2, 2), new Equipement("baton en chêne", 0, 12, 12, 0, 2, 3),
 		new Equipement("baguette en chêne", 0, 6, 6, 0, 1, 3), new Equipement("épée en acier", 12, 0, 0, 0, 1, 3), new Equipement("estramaçon en acier", 24, 0, 0, 0, 2, 3),
 		new Equipement("dague en acier", 9, 0, 0, 6, 1, 3), new Equipement("arc en chêne et flèches en acier", 9, 0, 0, 4, 2, 3)};
+		ArrayList<Monstre> bestiaire = new ArrayList<Monstre>();
+		ArrayList<Competence> listeCompetence = new ArrayList<Competence>();
+		Fichier.RecuperationConfig(bestiaire);
+		Fichier.LectureCompetence(listeCompetence);
+
 	}
 }
