976c0faa0a2111d92f5862925a1e8fa5c9842fdc
QuentinPavy <78421368+QuentinPavy@users.noreply.github.com>
2021-03-16 19:05:06 +0100

    Update Competence.java

--- src/Competence.java
+++ src/Competence.java
@@ -33,33 +33,38 @@ public class Competence {
     
     // comme on ne peut pas stocker des fonctions dans un String, on attribue un chiffre à chacune 
     // des fonctions existantes puis via cette fonction on calcule les dégâts en fonction de ce chiffre
-      public void Degat(Individu ind){
-        ArrayList<Integer> degat = new ArrayList<Integer>();
-        int d = this.degat;
-        String s = this.stat;
-        int c = this.cout;
+ public static ArrayList<Integer> Degat(Individu ind, Competence comp){                    
+        ArrayList<Integer> degat = new ArrayList<Integer>(); 
+        int d = comp.degat;
+        String s = comp.stat;
+        int c = comp.cout;
+        int rang = comp.rang;
         switch(d){
             case 1:
                 if(s == "FOR"){
-                    degat.add(ind.attaqueF());
+                    degat.add(ind.attaqueF(1));
+                    degat.add(ind.attaqueF(2));
                 }
                 if(s == "AGI"){
-                    degat.add(ind.attaqueA());
+                    degat.add(ind.attaqueA(1));
+                    degat.add(ind.attaqueA(2));
                 }
                 if(s == "SAG"){
-                    degat.add((int)(ind.sagesse*(this.rang/2)));
+                    degat.add((int)(ind.sagesse*(rang/2)));
                     degat = ind.attaqueM(degat);
                 }
                 break;
             case 2:
                 if(s == "FOR"){
-                    degat.add((int)((ind.attaqueF())*(((this.rang-1)*0.2)+1)));   
+                    degat.add((int)((ind.attaqueF(1))*(((rang-1)*0.2)+1)));
+                    degat.add((int)((ind.attaqueF(2))*(((rang-1)*0.2)+1)));     
                 } 
                 if(s == "AGI"){
-                    degat.add((int)((ind.attaqueA())*(((this.rang-1)*0.2)+1))); 
+                    degat.add((int)((ind.attaqueA(1))*(((rang-1)*0.2)+1))); 
+                    degat.add((int)((ind.attaqueA(2))*(((rang-1)*0.2)+1))); 
                 }
                 if(s == "SAG"){
-                    for(int i = 0; i<=this.rang; i++){
+                    for(int i = 0; i<=rang; i++){
                         degat.add(ind.sagesse/2);
                     }
                     degat = ind.attaqueM(degat);
@@ -68,35 +73,44 @@ public class Competence {
             case 3:
                 if(s == "FOR"){
                     while(c-- > 1){
-                        degat.add(ind.attaqueF());
+                        degat.add(ind.attaqueF(1));
+                        degat.add(ind.attaqueF(2));
                     }
-                    if((this.rang == 2)||(this.rang == 5)||(this.rang == 8)){
-                        degat.add((int)(ind.attaqueF()/2));
+                    if((rang == 2)||(rang == 5)||(rang == 8)){
+                        degat.add((int)(ind.attaqueF(1)/2));
+                        degat.add((int)(ind.attaqueF(2)/2));
                     }
-                    if((this.rang == 3)||(this.rang == 6)||(this.rang == 9)){
-                        degat.add((int)(ind.attaqueF()*0.8));
+                    if((rang == 3)||(rang == 6)||(rang == 9)){
+                        degat.add((int)(ind.attaqueF(1)*0.8));
+                        degat.add((int)(ind.attaqueF(2)*0.8));
                     }
-                    if((this.rang == 4)||(this.rang == 7)){
-                        degat.add(ind.attaqueF());
+                    if((rang == 4)||(rang == 7)){
+                        degat.add(ind.attaqueF(1));
+                        degat.add(ind.attaqueF(2));
                     }
                     break;
                 }
                 if(s == "AGI"){
                     while(c-- > 1){
-                        degat.add(ind.attaqueA());
+                        degat.add(ind.attaqueA(1));
+                        degat.add(ind.attaqueA(2));
                     }
-                    if((this.rang == 2)||(this.rang == 5)||(this.rang == 8)){
-                        degat.add((int)(ind.attaqueA()/2));
+                    if((rang == 2)||(rang == 5)||(rang == 8)){
+                        degat.add((int)(ind.attaqueA(1)/2));
+                        degat.add((int)(ind.attaqueA(2)/2));
                     }
-                    if((this.rang == 3)||(this.rang == 6)||(this.rang == 9)){
-                        degat.add((int)(ind.attaqueA()*0.8));
+                    if((rang == 3)||(rang == 6)||(rang == 9)){
+                        degat.add((int)(ind.attaqueA(1)*0.8));
+                        degat.add((int)(ind.attaqueA(2)*0.8));
                     }
-                    if((this.rang == 4)||(this.rang == 7)){
-                        degat.add(ind.attaqueA());
+                    if((rang == 4)||(rang == 7)){
+                        degat.add(ind.attaqueA(1));
+                        degat.add(ind.attaqueA(2));
                     }
                     break;
                 }
         }
-        this.DM =  degat;
+        comp.DM =  degat;
+        return degat;
     }
 }
