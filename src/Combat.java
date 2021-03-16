265c46b4261c0ea34ff3a9bdbdcd5f04ca3acc73
QuentinPavy <78421368+QuentinPavy@users.noreply.github.com>
2021-03-16 19:08:11 +0100

    Update Combat.java

--- src/Combat.java
+++ src/Combat.java
@@ -1,16 +1,16 @@
 import java.lang.Math;
 
 public class Combat {
-	//prend un deux types et un individu, un montant de dÃ©dégâts¢ts et un montant de perce-armure
-	//puis applique les dégâts selon les faiblesses
-	//on peut, peut-être, à la place de prendre un montant de perce-armure, prendre un individu ou autre chose pour le perce-armure
+	//prend un deux types et un individu, un montant de dÃƒÂ©dÃ©gÃ¢tsÂ¢ts et un montant de perce-armure
+	//puis applique les dÃ©gÃ¢ts selon les faiblesses
+	//on peut, peut-Ãªtre, Ã  la place de prendre un montant de perce-armure, prendre un individu ou autre chose pour le perce-armure
 	public static void combat (Individu p1, /*Individu p2,*/ int perceArmure, String type1, String type2, int degat) {
 		degat = faiblesse(type1, type2)*degat;
 		degat = Math.min(0, degat - Math.min(0, p1.armure - perceArmure)); //on peut toujours appliquer armure et le perce-armure autre part
 		p1.pv = p1.pv - degat;
 	}
 
-	//prend deux types et renvoie le facteur de dégâts correspondant
+	//prend deux types et renvoie le facteur de dÃ©gÃ¢ts correspondant
 	public static int faiblesse (String type1, String type2) {
 		switch(type1) {
 			case "lumiere":	if(type2.equals("lumiere")||type2.equals("tenebres")||type2.equals("physique")) return 1;
@@ -32,5 +32,25 @@ public class Combat {
 			case "physique": return 1;
 		}
 		return 1;
+	}
+	
+	// permet de d'apliquer les dÃ©gat subit en un tour (ne prend pas la res Ã©lÃ©mentaire car les monstre n'en on pas)
+	public static ArrayList<String> combat(Individu ind, Monstre m, Competence c){
+		ArrayList<Integer> DM = new ArrayList<Integer>();
+		DM = Competence.Degat(ind,c);
+		int n = DM.size()/2;
+		for(Equipement e: Individu.armeEquip(ind)){
+			for(int i = 0; i<n; i++){
+				if(0 < DM.get(i)-(m.PA-(c.perca+(e.perceArmure)))){
+					m.HP = m.HP-(DM.get(i)-(m.PA-(c.perca+(e.perceArmure))));
+					DM.remove(i);
+				}
+			}
+		}
+		if(m.PD-ind.armure > 0){
+			ind.pv = ind.pv-(m.PD-ind.armure);
+		}
+		ArrayList<String> res = new ArrayList<String>();
+		return(res);
 	}
 }
