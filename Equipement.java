116ebe0b29478da37912bd648bd0166ff4de72a9
QuentinPavy <78421368+QuentinPavy@users.noreply.github.com>
2021-03-16 16:14:46 +0100

    Update Equipement.java

--- Equipement.java
+++ Equipement.java
@@ -2,7 +2,7 @@ public class Equipement {
 	//caractéristiques de l'arme
 	public String nom;
 	//0 = pas de bonus; plus de 0: octroie du degat/soin supplémentaire pour certaines capacites
-	public int degatPhysique,degatMagique;
+	public int degatPhysique,degatMagique,armure;
 	//indique le nombre d'armure ignorer par l'arme
 	public int perceArmure;
 	public int nbMain;
@@ -15,9 +15,10 @@ public class Equipement {
       		nom = tab[0];
 		degatPhysique = Integer.parseInt(tab[1]);
 		degatMagique = Integer.parseInt(tab[2]);
-		perceArmure = Integer.parseInt(tab[3]);
-		nbMain = Integer.parseInt(tab[4]);
-		rang = Integer.parseInt(tab[5]);
-		type = tab[6];
+		armure = Integer.parsInt(tab[3]);
+		perceArmure = Integer.parseInt(tab[4]);
+		nbMain = Integer.parseInt(tab[5]);
+		rang = Integer.parseInt(tab[6]);
+		type = tab[7];
 	}
 }
