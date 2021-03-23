83016d297e7ab0f982d9662a6caab57340cf8020
Louison Crépin <77686027+vaddrep@users.noreply.github.com>
2021-03-23 16:43:13 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -11,5 +11,16 @@ public class Main {
 		Fichier.LectureCompetence(listeCompetence);
     
 		Fenetre f=new Fenetre();
+		Monstre m1 = new Monstre("test", 1, 2, 3);
+		System.out.println(m1.Nom);
+		int nbCombT = 4; //nombre de combat total à laisser?
+		Individu ind = null;
+		int nbCombat = 1; //numéro de combat
+		//déroulement du jeu
+		while((ind.pv >= 0)&&(nbCombat < 12)) {
+			Monstre m = SelectM(bestiaire, nbCombat);
+			//selection compétence
+			f.getParchemin().getTextArea().append(Combat.combat(ind, m, c));
+		}
 	}
 }
