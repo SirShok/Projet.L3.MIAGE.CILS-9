fccf62c2f79a77a1c1bc3378cdaa1868d0fdd9e1
SirShok <benCOMultra.44@hotmail.fr>
2021-03-25 13:58:44 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -9,11 +9,13 @@ import java.util.ArrayList;
 public class Main {
 	public static Individu joueur;
 	public static ArrayList<Monstre> bestiaire = new ArrayList<Monstre>();
+	public static Monstre adversaire = null;
 	public static ArrayList<Competence> listeCompetence = new ArrayList<Competence>();
 	public static void main(String[] args) throws FileNotFoundException, ClassNotFoundException, IOException, InterruptedException {
 		
 		Fichier.RecuperationConfig(bestiaire);
 		Fichier.LectureCompetence(Main.listeCompetence);
+		adversaire = Combat.SelectM(bestiaire, 0, 3);
 		try {
 		    GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
 		    ge.registerFont(Font.createFont(Font.TRUETYPE_FONT, new File("Autre/images/nine0.ttf")));
